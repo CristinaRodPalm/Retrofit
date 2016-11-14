ce6c9c3ca02df22c4a3f75b16c4ec246b7f05af0
Cristina <cristinarodriguez.palmer@gmail.com>
2016-11-14 20:15:10 +0100

    Rest asíncrono acabado con todas las nuevas querys

--- src/main/java/palmer/RestAsync.java
+++ src/main/java/palmer/RestAsync.java
@@ -2,6 +2,7 @@ package palmer;
 
 import palmer.domain.Player;
 import palmer.domain.Position;
+import palmer.domain.Statistic;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -9,7 +10,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by User on 12/11/2016.
@@ -104,5 +107,85 @@ public class RestAsync {
             }
         });
 
+        //      --> Práctica 2
+
+        // GET --> ORDER BY BASKETS DESC
+        Call<List<Player>> callOrderByBaskets = playerService.orderByBaskets();
+        callOrderByBaskets.enqueue(new Callback<List<Player>>() {
+            @Override
+            public void onResponse(Call<List<Player>> call, Response<List<Player>> response) {
+                System.out.println("Code: "+response.code()+"\n"
+                        +"ORDER BY BASKETS DESC: "+response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Player>> call, Throwable t) {
+                System.out.println("ORDER BY BASKETS DESC --> ERROR\n"+t.getMessage());
+            }
+        });
+
+        // GET --> BASKETS GREATER THAN EQUAL
+        Call<List<Player>> callBasketsGreaterThanEqual = playerService.basketsGreaterThanEqual(5);
+        callBasketsGreaterThanEqual.enqueue(new Callback<List<Player>>() {
+            @Override
+            public void onResponse(Call<List<Player>> call, Response<List<Player>> response) {
+                System.out.println("Code: "+response.code()+"\n"
+                        +"BASKETS GREATER THAN EQUAL: "+response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Player>> call, Throwable t) {
+                System.out.println("BASKETS GREATER THAN EQUAL --> ERROR\n"+t.getMessage());
+            }
+        });
+
+        // GET --> BASKETS BETWEEN
+        Call<List<Player>> callBasketsBetween = playerService.basketsBetween(2, 10);
+        callBasketsBetween.enqueue(new Callback<List<Player>>() {
+            @Override
+            public void onResponse(Call<List<Player>> call, Response<List<Player>> response) {
+                System.out.println("Code: "+response.code()+"\n"
+                        +"BASKETS BETWEEN: "+response.body());
+            }
+
+            @Override
+            public void onFailure(Call<List<Player>> call, Throwable t) {
+                System.out.println("BASKETS BETWEEN --> ERROR\n"+t.getMessage());
+            }
+        });
+
+        // GET --> AVG BASKETS, ASSISTS, REBOUND FROM ALL PLAYERS SAME POSITION
+
+        Call<Map<Position, Statistic>> callAvgByPosition = playerService.avgByPosition();
+        callAvgByPosition.enqueue(new Callback<Map<Position, Statistic>>() {
+            @Override
+            public void onResponse(Call<Map<Position, Statistic>> call, Response<Map<Position, Statistic>> response) {
+                Map<Position, Statistic> mapAvgByPosition= response.body();
+                System.out.println("Code: "+response.code()+"\n"
+                        +"STATISTICS PLAYERS SAME POSITION: "+mapAvgByPosition);
+            }
+
+            @Override
+            public void onFailure(Call<Map<Position, Statistic>> call, Throwable t) {
+                System.out.println("STATISTICS PLAYERS SAME POSITION --> ERROR\n"+t.getMessage());
+            }
+        });
+
+        // GET --> ALL PLAYERS SAME POSITION ORDERED BY BASKETS
+        Call<Map<Position, Collection<Player>>> callStatisticPosition = playerService.statisticFromPosition();
+        callStatisticPosition.enqueue(new Callback<Map<Position, Collection<Player>>>() {
+            @Override
+            public void onResponse(Call<Map<Position, Collection<Player>>> call, Response<Map<Position, Collection<Player>>> response) {
+                Map<Position, Collection<Player>> mapPositionPlayers= response.body();
+                System.out.println("ALL PLAYERS SAME POSITION ORDERED BY BASKETS \n Code:" +
+                        response.code()+"\n"+mapPositionPlayers);
+            }
+
+            @Override
+            public void onFailure(Call<Map<Position, Collection<Player>>> call, Throwable t) {
+                System.out.println("ALL PLAYERS SAME POSITION ORDERED BY BASKETS --> ERROR\n"+t.getMessage());
+
+            }
+        });
     }
 }
